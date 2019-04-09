6db4ccbaceb479b5e6ceb6be33121f79dfda3fd9
James Ku <thatoneddrguy@gmail.com>
2019-04-09 12:25:36 -0700

    (void*) comparison conversion

--- sender.cpp
+++ sender.cpp
@@ -54,7 +54,7 @@ void init(int& shmid, int& msqid, void*& sharedMemPtr)
 	
 	/* TODO: Attach to the shared memory */
 	sharedMemPtr = shmat(shmid, (void*)0, 0);
-	if(sharedMemPtr == -1)
+	if(sharedMemPtr == (void*) -1)
 	{
 		perror("shmat");
 		exit(1);
